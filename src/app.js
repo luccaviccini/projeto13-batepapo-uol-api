7a80a160067fde9c72abbc0d8f6526c9dcc49b1b
Lucca Viccini <luccaviccini@gmail.com>
2023-01-12 19:34:03 -0300

    feat: POST \messages complete

--- src/app.js
+++ src/app.js
@@ -29,17 +29,11 @@ db = mongoClient.db();
 const UsersCollection = db.collection("participants");
 const MessagesCollection = db.collection("messages");
 
-
-
-// ROTA DE LOGIN
-
-
-
+// post participants
 app.post('/participants', async (req, res) => {
-    
-
 
     const { name } = req.body;  
+
     // validade empty string joi 
     const schema = joi.object({
         name: joi.string().min(1).required(),
@@ -61,16 +55,54 @@ app.post('/participants', async (req, res) => {
       await db.collection("participants").insertOne({name, lastStatus: Date.now()});
       await db.collection("messages").insertOne({from: name, to: "Todos", text: "entra na sala...", type: "status", time:formatedTimestamp });
 
-      return res.status(201).send();
+      return res.status(201).send("Participante entrou na sala!");
     } catch (err) {
-      return res.status(422).send();
-    }
-    
-  
-  
-    
+      return res.status(422).send('O participante nao conseguiu entrar na sala');
+    } 
+
   })
-// ROTAS:
+// get all participants
+app.get('/participants', async (req, res) => {
+    const participants = await UsersCollection.find().toArray();
+    return res.status(200).send(participants);
+  });
+
+// post message
+app.post('/messages', async (req, res) => {
+    const {to, text, type } = req.body;
+    // get User from header
+    const from = req.headers.user;    
+    
+
+    let formatedTimestamp = dayjs(Date.now()).format('HH:mm:ss');
+
+    // validade empty string joi to and text
+    const schema = joi.object({
+        to: joi.string().min(1).required(),
+        text: joi.string().min(1).required(),
+        type: joi.string().valid('message', 'private_message').required(),
+    });
+    const { error } = schema.validate({ to, text, type });
+    const user = await UsersCollection.findOne({name: from})   
+    if (error || !user) {
+        return res.status(422).send("Erro na validação");
+    }
+
+    try {
+      await db.collection("messages").insertOne({from, to, text, type, time:formatedTimestamp });
+      return res.status(201).send("Mensagem enviada!");
+    } catch (err) {
+      return res.status(422).send('A mensagem nao foi enviada');
+    }
+  });
+
+// get all messages
+app.get('/messages', async (req, res) => {
+    const messages = await MessagesCollection.find().toArray();
+    return res.status(200).send(messages);
+  });
+
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
