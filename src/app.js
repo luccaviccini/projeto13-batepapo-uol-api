eab8f1a29924b67e448f832b515cfc534f9719e2
Lucca Viccini <luccaviccini@gmail.com>
2023-01-12 11:13:39 -0300

    feat: post participants and validation using JOI (not empty name)

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.use(express.json());
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
+//const users = [{name: 'João'}]; // O conteúdo do lastStatus será explicado nos próximos requisitos
+const messages = [{from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}];
+
+
 try {
 await mongoClient.connect();
 } catch (err) {
@@ -21,7 +25,11 @@ console.log("Erro no mongo.conect", err.message);
 }
 
 db = mongoClient.db();
-const talCollection = db.collection("test");
+const UserCollection = db.collection("participants");
+
+UserCollection.insertOne({ name: "João" });
+
+app.post
 
 app.get("/test", (req, res) => {
     console.log("Entrei na rota teste");
@@ -29,7 +37,25 @@ app.get("/test", (req, res) => {
     
 });
 
+app.post('/participants', (req, res) => {
+    const { name } = req.body;  
+    // validade empty string joi 
+    const schema = joi.object({
+        name: joi.string().min(1).required(),
+    });
+    const { error } = schema.validate({ name });
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }   
+    db.collection("participants").insertOne({name})
+      .then(() => {
+        return res.status(201).send("Participante salvo com sucesso!")
+      })
+      .catch(() => {
+        res.status(422).send('O participante não foi salvo!')
+      });
+  })
 // ROTAS:
 
-const port = 5000;
-app.listen(port, () => console.log(`Server running in port: ${port}`));
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running in port: ${PORT}`));
